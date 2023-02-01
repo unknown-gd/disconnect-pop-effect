41a9cf9db0721cdeea729fa39df84732e0b20d01
PrikolMen <olegis019@gmail.com>
2023-02-01 13:47:03 +0400

    Code Improve

--- lua/effects/balloon_pop.lua
+++ lua/effects/balloon_pop.lua
@@ -5,27 +5,21 @@ local math_Rand = math.Rand
 local Vector = Vector
 local Angle = Angle
 
-local NumParticles = 32
+local particlesCount = 32
 local texture = "particles/balloon_bit"
 local sound_path = "garrysmod/balloon_pop_cute.wav"
 
 function EFFECT:Init( data )
+	local pos = data:GetOrigin()
+	local color = data:GetStart()
+	sound_Play( sound_path, pos, 90, math_random( 90, 120 ) )
 
-	local vOffset = data:GetOrigin()
-	local Color = data:GetStart()
-
-	sound_Play( sound_path, vOffset, 90, math_random( 90, 120 ) )
-
-	local emitter = ParticleEmitter( vOffset, true )
-
-	for i = 0, NumParticles do
-
-		local Pos = Vector( math_Rand( -1, 1 ), math_Rand( -1, 1 ), math_Rand( -1, 1 ) )
-
-		local particle = emitter:Add( texture, vOffset + Pos * 8 )
-		if ( particle ) then
-
-			particle:SetVelocity( Pos * 500 )
+	local emitter = ParticleEmitter( pos, true )
+	for i = 0, particlesCount do
+		local vel = Vector( math_Rand( -1, 1 ), math_Rand( -1, 1 ), math_Rand( -1, 1 ) )
+		local particle = emitter:Add( texture, pos + vel * 8 )
+		if (particle) then
+			particle:SetVelocity( vel * 500 )
 
 			particle:SetLifeTime( 0 )
 			particle:SetDieTime( 10 )
@@ -33,8 +27,7 @@ function EFFECT:Init( data )
 			particle:SetStartAlpha( 255 )
 			particle:SetEndAlpha( 255 )
 
-			local Size = math_Rand( 1, 3 )
-			particle:SetStartSize( Size )
+			particle:SetStartSize( math_Rand( 1, 3 ) )
 			particle:SetEndSize( 0 )
 
 			particle:SetRoll( math_Rand( 0, 360 ) )
@@ -43,8 +36,8 @@ function EFFECT:Init( data )
 			particle:SetAirResistance( 100 )
 			particle:SetGravity( Vector( 0, 0, -300 ) )
 
-			local RandDarkness = math_Rand( 0.8, 1.0 )
-			particle:SetColor( Color.r * RandDarkness, Color.g * RandDarkness, Color.b * RandDarkness )
+			local darkness = math_Rand( 0.8, 1.0 )
+			particle:SetColor( color.r * darkness, color.g * darkness, color.b * darkness )
 
 			particle:SetCollide( true )
 
@@ -52,9 +45,7 @@ function EFFECT:Init( data )
 
 			particle:SetBounce( 1 )
 			particle:SetLighting( true )
-
 		end
-
 	end
 
 	emitter:Finish()
