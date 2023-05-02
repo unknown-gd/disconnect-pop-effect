a7f9438aad223b7fbc363297df1a24dc12022a57
PrikolMen <olegis019@gmail.com>
2023-05-02 23:51:37 +0400

    Updated

--- lua/autorun/sh_disconnect_pop_effect.lua
+++ lua/autorun/sh_disconnect_pop_effect.lua
@@ -1,6 +1,3 @@
-if game.SinglePlayer() then return end
-local addonName = 'Disconnect Effect - Balloon Pop'
-
 /*
 
     Title: Disconnect Effect - Balloon Pop
@@ -9,35 +6,37 @@ local addonName = 'Disconnect Effect - Balloon Pop'
 
 */
 
-if (SERVER) then
+if game.SinglePlayer() then return end
+local addonName = "Disconnect Effect - Balloon Pop"
+
+if SERVER then
+
+    local player_GetCount = player.GetCount
+    local EffectData = EffectData
+    local util = util
+    local net = net
 
     util.AddNetworkString( addonName )
 
-    local player_GetCount = player.GetCount
-    local util_IsInWorld = util.IsInWorld
-    local util_Effect = util.Effect
-    local net_SendPVS = net.SendPVS
-    local EffectData = EffectData
-    local net_Start = net.Start
-
-    hook.Add('PlayerDisconnected', addonName, function( ply )
+    hook.Add( "PlayerDisconnected", addonName, function( ply )
         if ply:IsListenServerHost() then return end
-        if (player_GetCount() < 2) then return end
+        if player_GetCount() < 2 then return end
 
         local pos = ply:LocalToWorld( ply:OBBCenter() )
-        if util_IsInWorld( pos ) then
-            local fx = EffectData()
-            fx:SetOrigin( pos )
-            fx:SetScale( 10 )
-            fx:SetStart( ply:GetPlayerColor() * 255 )
-            util_Effect( 'balloon_pop', fx, true, true )
-            net_Start( addonName )
-            net_SendPVS( pos )
-        end
-    end)
+        if not util.IsInWorld( pos ) then return end
+
+        local fx = EffectData()
+        fx:SetOrigin( pos )
+        fx:SetScale( 10 )
+        fx:SetStart( ply:GetPlayerColor() * 255 )
+        util.Effect( "balloon_pop", fx, true, true )
+
+        net.Start( addonName )
+        net.SendPVS( pos )
+    end )
 
 end
 
-if (CLIENT) then
+if CLIENT then
     net.Receive( addonName, achievements.BalloonPopped )
 end
